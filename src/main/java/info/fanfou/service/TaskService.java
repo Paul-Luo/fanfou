e73bfec4b32cb61c9648dd08f3e296f52a0d9a47
chaoluo <chaoluo@cisco.com>
2015-12-23 14:01:11 +0800

    打印时间

--- src/main/java/info/fanfou/service/TaskService.java
+++ src/main/java/info/fanfou/service/TaskService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * author : chaoluo
  * date : 2015/12/21
@@ -18,6 +21,20 @@ public class TaskService {
     @Scheduled(cron = "30 13 * * 5 ?")
     public void sendBill() {
         logger.info("begin to send email!");
+
+//        SimpleMailMessage smm = new SimpleMailMessage();
+//        smm.setFrom("Hello");
+//        smm.setTo("234750677@qq.com");
+//        smm.setSubject("Hello world");
+//        smm.setText("Hello world via spring mail sender");
+//        // 发送邮件
+//        mailSender.send(smm);
+    }
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void logTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        logger.debug("Now is", dateFormat.format(new Date()).toString());
 
 //        SimpleMailMessage smm = new SimpleMailMessage();
 //        smm.setFrom("Hello");
